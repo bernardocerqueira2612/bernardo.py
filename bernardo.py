06b3017812dd119d8e454e021663b80bbe20b75c
bernardocerqueira2612 <159343790+bernardocerqueira2612@users.noreply.github.com>
2024-03-26 20:27:39 -0300

    Add files via upload

---
+++ bernardo.py
@@ -0,0 +1,16 @@
+print("Bernardo")
+
+numero = 10
+nome = "Pedro"
+booleano = True
+
+resposta = (2 < 3) or (4 + 5 and numero ! = 10)
+
+lista = [Parenteses, Expoentes, Multiplicacoes e divisoes (da esquerda para a direita) e Somas e subtracoes (da esquerda para a direita)]
+lista_ordenada = sorted(lista)
+print(lista_ordenada)  # Saída: [Parenteses, Expoentes, Multiplicacoes e divisoes (da esquerda para a direita), Somas e subtracoes (da esquerda para a direita)]
+
+
+lista = [D, B, A, C]
+lista.sort()
+print(lista)  # Saída: [A, B, C, D]
